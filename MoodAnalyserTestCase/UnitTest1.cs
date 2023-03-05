b197dc595e3a0db368a0ea4d8535d4e402fca1a0
akshata <akshata2018p@gamil.com>
2023-03-05 22:18:16 +0530

    [akshata]Add:Mood Analyse TestCase 1.2

--- MoodAnalyserTestCase/UnitTest1.cs
+++ MoodAnalyserTestCase/UnitTest1.cs
@@ -4,12 +4,20 @@ namespace MoodAnalyserTestCase
     public class Tests
     {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
+        private object moodAnalyzer;
+
         [Test]
         public void GivenSadMood_WhenAnalysed_ShouldReturnSad()
         {
             string result = moodAnalyser.AnalyseMood("I Am In Sad Mood.");
             Assert.AreEqual(result, "Sad");
         }
-    
+        [Test]
+        public void GivenHappyMood_WhenAnalyzed_ShouldReturnHappy()
+        {
+            string result = moodAnalyzer.AnaylseMood("I Am In Happy Mood.");
+            Assert.AreEqual(result, "Happy");
+        }
+
     }
 }
